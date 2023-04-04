1025b1a81a12bb1968b17ef83cd2d22b0977336e
Lisa ♡ <stkhalisha@gmail.com>
2023-04-04 16:26:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
+// definisikan 3 variable untuk menyimpan 2 angka dan 1 operator
 let prevNumber = "";
 let calculationOperator = "";
 let currentNumber = "0";
 
-const inputNumber = (number) => {
-  if (currentNumber === "0") {
-    currentNumber = number;
-  } else {
-    currentNumber += number;
-  }
-};
-
+// membuat agar layar dapat menampilkan angka menggunakan method querySelector
 const calculatorScreen = document.querySelector(".calculator-screen");
 const updateScreen = (number) => {
   calculatorScreen.value = number;
 };
 
+// mengambil element menggunakan querySelectorAll
 const numbers = document.querySelectorAll(".number");
 numbers.forEach((number) => {
   number.addEventListener("click", (event) => {
@@ -23,6 +18,15 @@ numbers.forEach((number) => {
   });
 });
 
+// definisikan function “inputNumber”
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
 const operators = document.querySelectorAll(".operator");
 operators.forEach((operator) => {
   operator.addEventListener("click", (event) => {
@@ -30,6 +34,7 @@ operators.forEach((operator) => {
   });
 });
 
+// definisikan function “inputOperator”
 const inputOperator = (operator) => {
   if (calculationOperator === "") {
     prevNumber = currentNumber;
@@ -38,12 +43,14 @@ const inputOperator = (operator) => {
   currentNumber = "0";
 };
 
+// mengambil element menggunakan querySelector
 const equalSign = document.querySelector(".equal-sign");
 equalSign.addEventListener("click", () => {
-  calculate();
+  calculate(); // Jalankan Function calculate saat = diclick
   updateScreen(currentNumber);
 });
 
+// definisikan function “calculate”
 const calculate = () => {
   let result = "";
   switch (calculationOperator) {
@@ -66,15 +73,17 @@ const calculate = () => {
   calculationOperator = "";
 };
 
+// Definisikan function “clearAll”
 const clearAll = () => {
   prevNumber = "";
   calculationOperator = "";
   currentNumber = "0";
 };
 
+// mengambil element menggunakan querySelector
 const clearBtn = document.querySelector(".all-clear");
 clearBtn.addEventListener("click", () => {
-  clearAll();
+  clearAll(); // Jalankan Function clearAll saat tombol AC diclick
   updateScreen(currentNumber);
 });
 
